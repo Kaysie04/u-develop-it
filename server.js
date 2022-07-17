05e312de0f845760ff60afcf01aaaba0f5794d5b
Kaysie <kaysielandersonwork@gmail.com>
2022-07-17 18:51:12 -0500

    built first database call for * from candidates

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3001
+const mysql = require('mysql2')
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+ 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username,
+        user: 'root',
+        // your MySQL password
+        password: 'Milo04022021!',
+        database: 'election'
+    },
+    console.log('Connected to the election database. yeaaaa boyyyy!!!!')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows)
+})
 
 
 
